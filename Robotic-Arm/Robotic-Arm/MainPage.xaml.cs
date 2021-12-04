79d20c04669867824430f91c906d5a7163a13e8c
Zachary Kunkel <zacharyjkunkel@gmail.com>
2021-12-03 23:49:49 -0600

    Add project files.

---
+++ Robotic-Arm/Robotic-Arm/MainPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Robotic_Arm
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+
+        async void OnButtonClicked(object sender, EventArgs args)
+        {
+            await label.RelRotateTo(360, 1000);
+            //OINK
+        }
+    }
+}
