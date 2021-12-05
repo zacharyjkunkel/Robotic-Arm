40f91a9d50d64dec6240178543456bf325884f71
Zachary Kunkel <zacharyjkunkel@gmail.com>
2021-12-05 15:54:25 -0600

    bt testing

--- Robotic-Arm/Robotic-Arm/MainPage.xaml.cs
+++ Robotic-Arm/Robotic-Arm/MainPage.xaml.cs
@@ -6,6 +6,10 @@ using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
 
+using Plugin.BLE;
+using Plugin.BLE.Abstractions.Contracts;
+using System.Diagnostics;
+
 namespace Robotic_Arm
 {
     public partial class MainPage : ContentPage
@@ -15,11 +19,25 @@ namespace Robotic_Arm
             InitializeComponent();
         }
 
-
+        //scans for bt devices
         async void OnButtonClicked(object sender, EventArgs args)
         {
-            await label.RelRotateTo(360, 1000);
+            await label.RelRotateTo(360, 2000);
             //OINK
+            var ble = CrossBluetoothLE.Current;
+            var adapter = CrossBluetoothLE.Current.Adapter;
+            var state = ble.State;
+            Debug.WriteLine("STATE: " + state);
+
+
+            adapter.ScanMode = Plugin.BLE.Abstractions.Contracts.ScanMode.Balanced;
+            await adapter.StartScanningForDevicesAsync();
+
+
+
+            
+            state = ble.State;
+            Debug.WriteLine("STATE: " + state);
         }
     }
 }
